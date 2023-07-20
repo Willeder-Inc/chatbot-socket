c2cc320e8664cd65770fc043ff0e5ab152bba660
rorikata <28458102+rorikata@users.noreply.github.com>
2023-07-20 22:22:55 +0900

    refactoring: add axios and remove fetch

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,20 +1,21 @@
-import React, { useState, useEffect, useRef } from "react"
-import "./Chat.css"
-import talkVideo from "./assests/talk.mp4"
+import React, { useState, useEffect, useRef } from 'react'
+import './Chat.css'
+import talkVideo from './assests/talk.mp4'
+import axios from 'axios'
 
 interface Message {
   content: string
-  sender: "sender" | "receiver"
+  sender: 'sender' | 'receiver'
 }
 
 const ChatApp: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([
-    { content: "Hello", sender: "receiver" },
-    { content: "Hi there!", sender: "sender" },
-    { content: "How are you?", sender: "receiver" },
+    { content: 'Hello', sender: 'receiver' },
+    { content: 'Hi there!', sender: 'sender' },
+    { content: 'How are you?', sender: 'receiver' },
   ])
 
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState('')
   const [isVideoLoaded, setIsVideoLoaded] = useState(false)
   const typingAnimationRef = useRef<HTMLDivElement>(null)
 
@@ -24,22 +25,25 @@ const ChatApp: React.FC = () => {
 
   const getResponse = async (inputValue: any) => {
     try {
-      const formData = new FormData()
-      formData.append("question", inputValue)
-      // const response: any = await fetch("http://172.104.206.85/chatbot", {
-      const response: any = await fetch("http://127.0.0.1:5000/chatbot", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        // body: JSON.stringify({ question: inputValue }),
-        body: formData,
-      })
-      const data = await response.json()
+      const response = await axios.post('http://127.0.0.1:5000/chatbot', { question: inputValue })
+      // const formData = new FormData()
+      // formData.append('question', inputValue)
+      // // const response: any = await fetch("http://172.104.206.85/chatbot", {
+      // const response: any = await fetch('http://127.0.0.1:5000/chatbot', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   // body: JSON.stringify({ question: inputValue }),
+      //   body: formData,
+      // })
+
+      const data = await response.data
+
       console.log(data)
       const newMessage: Message = {
-        content: data.data.message,
-        sender: "receiver",
+        content: data,
+        sender: 'receiver',
       }
 
       setMessages((prevInput) => [...prevInput, newMessage])
@@ -51,22 +55,22 @@ const ChatApp: React.FC = () => {
   }
 
   const handleSendMessage = async () => {
-    if (inputValue.trim() === "") {
+    if (inputValue.trim() === '') {
       return
     }
     const newMessage: Message = {
       content: inputValue,
-      sender: "sender",
+      sender: 'sender',
     }
 
     setMessages((prevInput) => [...prevInput, newMessage])
     getResponse(inputValue)
-    setInputValue("")
+    setInputValue('')
   }
 
   useEffect(() => {
     if (typingAnimationRef.current && !isVideoLoaded) {
-      typingAnimationRef.current.classList.add("typing-animation")
+      typingAnimationRef.current.classList.add('typing-animation')
     }
   }, [isVideoLoaded])
 
@@ -75,20 +79,17 @@ const ChatApp: React.FC = () => {
   }
 
   return (
-    <div className="chat-app">
-      <div className="message-list">
+    <div className='chat-app'>
+      <div className='message-list'>
         {messages.map((message, index) => (
           <div
             key={index}
-            className={`message-item ${
-              message.sender === "sender" ? "sender" : "receiver"
-            }`}
-          >
-            {message.sender === "receiver" && (
+            className={`message-item ${message.sender === 'sender' ? 'sender' : 'receiver'}`}>
+            {message.sender === 'receiver' && (
               <>
                 <video
-                  width="74"
-                  height="68"
+                  width='74'
+                  height='68'
                   // src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm"
                   src={talkVideo}
                   // controls
@@ -96,16 +97,16 @@ const ChatApp: React.FC = () => {
                 />
               </>
             )}
-            <div className="message-content">{message.content}</div>
+            <div className='message-content'>{message.content}</div>
           </div>
         ))}
       </div>
-      <div className="input-area">
+      <div className='input-area'>
         <input
-          type="text"
+          type='text'
           value={inputValue}
           onChange={handleInputChange}
-          placeholder="Type your message..."
+          placeholder='Type your message...'
         />
         <button onClick={handleSendMessage}>Send</button>
       </div>
