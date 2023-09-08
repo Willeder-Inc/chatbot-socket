6494dc2b2740526f3525df201b10b99fd86e4726
aravinthraajk <aravinthraajk@gmail.com>
2023-09-09 01:17:53 +0530

    video updated

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -5,14 +5,21 @@ import "./MainApp.css"
 import { Link, useNavigate } from "react-router-dom"
 import AI from "./assests/AI_latest.mp4"
 import BG from "./assests/futuristic_bg.mp4"
+import ChatApp from "./ChatApp"
 
 const MainApp = () => {
   const [showControls, setshowControls] = useState(false)
   const [showPlayButton, setshowPlayButton] = useState(true)
+  const [showApp, setshowApp] = useState(false)
 
   let navigate = useNavigate()
   const myCallback = () => {
     navigate("/chat")
+  }
+
+  const showChatapp = () => {
+    setshowApp(true)
+    setshowControls(false)
   }
 
   const playVideo = () => {
@@ -24,8 +31,8 @@ const MainApp = () => {
   }
 
   return (
-    <div className="mainApp">
-      {!showControls ? (
+    <div className={`mainApp ${!showApp ? "black": null}`}>
+      {!showApp && !showControls ? (
         <div className="videoContainer">
           <video
             width="100%"
@@ -51,19 +58,20 @@ const MainApp = () => {
           </div>
         </div>
       ) : null}
-      {showControls ? (
+      {!showApp && showControls ? (
         <div className="videoContainer">
           <video width="100%" height="100%" muted autoPlay playsInline loop>
             <source src={BG} type="video/mp4"></source>
           </video>
           <div className="controlWrapper">
-            <Link to={"/chat"}>
+            {/* <Link to={"/chat"}>
               <button>Activate I Producer</button>
-            </Link>
-            {/* <button onClick={myCallback}>Activate I Producer</button> */}
+            </Link> */}
+            <button onClick={showChatapp}>Activate I Producer</button>
           </div>
         </div>
       ) : null}
+      {showApp ? <ChatApp /> : null}
     </div>
   )
 }
